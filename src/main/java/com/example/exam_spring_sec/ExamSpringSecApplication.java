9d6cc5c3ee2b0d496f70c906b9c26e4dea4935f1
Shoxrux <woxrux6070@gmail.com>
2022-04-08 11:59:31 +0500

    firstCommit

---
+++ src/main/java/com/example/exam_spring_sec/ExamSpringSecApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_spring_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSpringSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSpringSecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exam_spring_sec/ExamSpringSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_spring_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSpringSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
